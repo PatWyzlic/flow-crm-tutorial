0ff8b443d1f45fe3c9c73ecc9d9a3f884d3bd6a5
patwyzlic <patrickwyzlic@gmail.com>
2022-09-24 15:28:56 -0400

    Testing out button with notification pop up of name

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,11 +1,18 @@
 package com.example.application.views.list;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin.Horizontal;
+
+import aj.org.objectweb.asm.Label;
 
 @PageTitle("list")
 @Route(value = "")
@@ -13,6 +20,17 @@ public class ListView extends VerticalLayout {
 
     public ListView() {
         add(new H2("Initial text"));
+
+        Button button = new Button("Click me");
+        TextField name = new TextField("name");
+
+        HorizontalLayout h1 = new HorizontalLayout(name, button);
+        h1.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+
+        button.addClickListener(click-> Notification.show("Hello, " + name.getValue()));
+
+        add(h1);
+
     }
 
 }
